88d3999d6874fe62eb1565aeee0c80c544d80575
Gladrat <3368584+Gladrat@users.noreply.github.com>
2024-10-01 14:31:16 +0200

    update

--- cours/01.bases.py
+++ cours/01.bases.py
@@ -1,1 +1,23 @@
 # ici les bases....
+
+print("hello !")
+
+n = 42
+n = n + 1
+n += 1
+
+n = n - 1
+n -= 1
+
+# ICI !
+n = n * 10
+n = n / 10
+
+n = 20//3
+n = 20%3
+
+# ---
+
+age = 39
+print(f"J'affiche mon age: {age}")
+
